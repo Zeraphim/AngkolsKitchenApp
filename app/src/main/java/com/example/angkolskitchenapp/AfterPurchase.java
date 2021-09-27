9d8bed93a834e1edf4d23c6e0d934360507edc42
Mark Cortez <markcortez2002@gmail.com>
2021-09-27 20:57:56 +0800

    activity_after_purchase.xml and activity_place_order.xml updated layout constraints | AfterPurchase.java, FoodCart.java, PlaceOrder.java activities connected

--- app/src/main/java/com/example/angkolskitchenapp/AfterPurchase.java
+++ app/src/main/java/com/example/angkolskitchenapp/AfterPurchase.java
@@ -2,13 +2,21 @@ package com.example.angkolskitchenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AfterPurchase extends AppCompatActivity {
+
+    ImageButton backToMainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_purchase);
+        backToMainButton = findViewById(R.id.backToMainButton);
+        backToMainButton.setOnClickListener(view -> {
+            startActivity(new Intent(AfterPurchase.this, MainActivity.class));
+        });
     }
 }
