5d9f67bbf87d3cdb7e0c361f1f89426c319d9517
Zeraphim <diamantejc87@gmail.com>
2021-09-30 02:01:21 +0800

    Added onboarding, migrated gallery page to another activity

---
+++ app/src/main/java/com/example/angkolskitchenapp/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.angkolskitchenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        }
+    }
+}
--- app/src/main/java/com/example/angkolskitchenapp/MainActivity.java
+++ app/src/main/java/com/example/angkolskitchenapp/MainActivity.java
@@ -1,40 +1,130 @@
 package com.example.angkolskitchenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogOut;
-    FirebaseAuth mAuth;
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
+    private MaterialButton buttonOnboardingAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogOut = findViewById(R.id.btnLogout);
-        mAuth = FirebaseAuth.getInstance();
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicators);
+        buttonOnboardingAction = findViewById(R.id.buttonOnboardingAction);
 
-        btnLogOut.setOnClickListener(view ->{
-            mAuth.signOut();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        setupOnboardingItems();
+
+        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
+
+        buttonOnboardingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onboardingViewPager.getCurrentItem() + 1 < onboardingAdapter.getItemCount()) {
+                    onboardingViewPager.setCurrentItem(onboardingViewPager.getCurrentItem() + 1);
+                } else {
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                    finish();
+                }
+            }
         });
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user == null){
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    private void setupOnboardingItems() {
+
+        List<OnboardingItem> onboardingItems = new ArrayList<>();
+
+        OnboardingItem itemPayOnline = new OnboardingItem();
+        itemPayOnline.setTitle("Pay Your Bill Online");
+        itemPayOnline.setDescription("Electric bill payment is a feature of online, mobile and telephone banking.");
+        itemPayOnline.setImage(R.drawable.pay_online);
+
+        OnboardingItem itemOnTheWay = new OnboardingItem();
+        itemOnTheWay.setTitle("Your Food Is On The Way");
+        itemOnTheWay.setDescription("Our delivery rider is on the way to deliver your order.");
+        itemOnTheWay.setImage(R.drawable.on_the_way);
+
+        OnboardingItem itemEatTogether = new OnboardingItem();
+        itemEatTogether.setTitle("Eat Together");
+        itemEatTogether.setDescription("Enjoy your meal and have a great day. Don't forget to rate us.");
+        itemEatTogether.setImage(R.drawable.eat_together);
+
+        onboardingItems.add(itemPayOnline);
+        onboardingItems.add(itemOnTheWay);
+        onboardingItems.add(itemEatTogether);
+
+        onboardingAdapter = new OnboardingAdapter(onboardingItems);
+    }
+
+    private void setupOnboardingIndicators() {
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_active
+            ));
+
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setCurrentOnboardingIndicator(int index) {
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
+
+        if (index == onboardingAdapter.getItemCount() - 1) {
+            buttonOnboardingAction.setText("Start");
+        } else {
+            buttonOnboardingAction.setText("Next");
         }
     }
 }
